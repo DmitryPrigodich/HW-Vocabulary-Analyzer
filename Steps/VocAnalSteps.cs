5dc2cbd0b98fcd940a0fcbf2c79f00c370a964b3
DmitryPrigodich <dmitrijs.prigodics@gmail.com>
2025-03-07 19:42:55 +0200

    Initiating thinking process

--- Steps/VocAnalSteps.cs
+++ Steps/VocAnalSteps.cs
@@ -7,19 +7,65 @@ namespace HW_Vocabulary_Analyzer.Steps;
 [Binding]
 public class VocAnalSteps
 {
-    private readonly TestLoginPage _loginPage;
-
-    public VocAnalSteps(ScenarioContext context)
+    [Given(@"^(.+) vocabulary (parts|vowels|consonants|structures) are analyzed$")]
+    public async Task AnalyzeVocabularyStep(string game, string whatToAnalyze)
     {
-        var page = (IPage)context["Page"];
-        _loginPage = new TestLoginPage (page);
+        switch (whatToAnalyze)
+        {
+            case "parts":
+                AnalyzeVocabularyParts();
+                break;
+            case "vowels": 
+                AnalyzeVocabularyLetters();
+                break;
+            case "consonants":
+                AnalyzeVocabularyLetters();
+                break;
+            case "structures":
+                AnalyzeVocabularyStructures();
+                break;
+            default:
+                break;
+        }
     }
-    
-    [Given(@"HW""(.*)"" vocabulary is analyzed")]
-    public async Task LoginWithCsv(string gamePart)
+
+    private void AnalyzeVocabularyParts()
     {
-        var vocabulary = Utils.CsvHelper.ReadCsv<UserData>("Data/HW1_Vocabulary.csv");
-        var admin = vocabulary.First(u => u.Parts == "Parts");
-        await _loginPage.Login(user.Username, user.Password);
+        //PARTS ANALYSIS
+        //get data from HW1_Vocabulary.csv
+        //go row by row, gather all the parts from Parts column
+        //print unique Parts list: part as a heading, and a list of words that have this part from Derivative column under that heading
+        //print it in Reports/HW1_Word_Parts.txt
+        //like this:
+        // AL
+        // Albegiido, Gaalsien, Hraal, Koraal... etc
+    }
+
+    private void AnalyzeVocabularyLetters()
+    {
+        //LETTER ANALYSIS
+        //get data from HW1_Vocabulary.csv
+        //go row by row, gather all the Vowels from Vowels column, separated by comma
+        //type a list of unique vowels
+        //count how many times vowel is met and write it as
+        //%VOWEL%: X times
+        
+        //get data from HW1_Vocabulary.csv
+        //go row by row, gather all the Consonants from Consonants column, separated by comma
+        //type a list of unique vowels
+        //count how many times vowel is met and write it as
+        //%VOWEL%: X times
+    }
+
+    private void AnalyzeVocabularyStructures()
+    {
+        //STRUCTURE ANALYSIS
+        //get data from HW1_Vocabulary.csv
+        //go row by row, gather all the Structure from Structure column
+        //print unique Structure list: part as a heading, and a list of words that have this Structure from Derivative column under that heading
+        //print it in Reports/HW1_Word_Parts.txt
+        //like this:
+        // XY
+        // Sha,Ra,Sa... etc
     }
 }
